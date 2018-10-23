432c104045a7ad0484e8e5fc9e9c557613188c16
yair Weiss <yair.we21@gmail.com>
2018-10-23 12:33:54 +0300

    commit3

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
 'use strict'
 
-function init (){
+function init() {
     renderImgs(gImgs);
+}
+
+
+function renderImgs(gImgs) {
+    var elImgsContainer = document.querySelector('.imgs-container')
+    var strHtmls = ''
+    for (let i = 0; i < gImgs.length; i++) {
+        var strHtml =
+            ` <img src="${getImgUrl()}" alt="Img Here" onclick = "openModal(this)">`
+        strHtmls += strHtml;
+    }
+    elImgsContainer.innerHtml = strHtmls;
+}
+
+function getImgUrl(){
+    
 }
 
 
