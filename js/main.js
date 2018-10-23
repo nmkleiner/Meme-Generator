d22c5d03e73335ac1bd389a54acaf59e861f33c7
Noam Kleiner <nmkleiner@gmail.com>
2018-10-23 13:33:32 +0300

    modal

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 'use strict'
 
-var gScreenSizes = {}
+var gCanvas
+var gCtx
 
 function init (){
-    gScreenSizes = getScreenSizes()
     renderImgs(gImgs);
+    initCanvas()
 }
 
+function initCanvas() {
+    gCanvas = document.querySelector('#canvas');
+    gCanvas.width = window.innerWidth - 10//calc something real instead
+    gCanvas.height = window.innerHeight - 10
+    gCtx = canvas.getContext('2d');
+}
 
 function renderImgs(gImgs) {
     var elImgsContainer = document.querySelector('.imgs-container')
@@ -34,8 +41,23 @@ function onGalleryImgClick(elImg) {
 
 function openModal() {
     $('.modal').slideToggle(400)
+    
 }
 
 function renderCanvas(elImg) {
-    
+    var imgSrc = getImageSrc(elImg)
+    console.log('imgSrc')
+    drawImage(imgSrc)
+}
+
+function getImageSrc(elImg) {
+    return elImg.getAttribute('src')
+}
+
+function drawImage(imgSrc) {
+    var img = new Image()
+    img.src = imgSrc
+    img.onload = function() {
+        gCtx.drawImage(img, 0, 0 ,gCanvas.width, gCanvas.height)
+    }
 }
