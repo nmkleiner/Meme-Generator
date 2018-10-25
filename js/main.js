79833fb8a24bca747dcfbaba28c22ae70b51a7dd
Noam Kleiner <nmkleiner@gmail.com>
2018-10-25 10:15:21 +0300

    pull

--- js/main.js
+++ js/main.js
@@ -124,10 +124,12 @@ function createImg(imgSrc) {
     img.src = imgSrc
     return img
 }
+
 function onResize() {
     setCanvasSize()
     drawImage(gCurrImg)
 }
+
 function setCanvasSize() {
     var heightFactor = gCurrImg.height / gCurrImg.width
 
